52096af88837c00284d9d251320f63b36d182672
贺哲 <hezhe1688@163.com>
2020-01-19 16:43:11 +0800

    动态路由

--- vuecli/src/router/index.js
+++ vuecli/src/router/index.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import home from '../views/Home.vue'
 import about from '../views/About'
+import user from '../views/User'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    redirect: 'home'   //配置默认进入首页
+  },
   {
     path: '/home',
     component: home
@@ -12,13 +17,18 @@ const routes = [
   {
     path: '/about',
     component: about
+  },
+  {
+    path: '/user/:userId',
+    component: user
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'history',   //取消默认采用的哈希值
   base: process.env.BASE_URL,
-  routes
+  routes,
+
 })
 
 export default router
---
+++ vuecli/src/views/User.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>我是User</h2>
+        <h3>{{$route.params.userId}}</h3>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "User"
+	}
+</script>
+
+<style scoped>
+
+</style>
